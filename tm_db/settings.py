7402df6a87b5a28ea46fbc6bf18cea44f5cac84c
bmokr <56951744+bmokr@users.noreply.github.com>
2024-01-27 00:53:03 +0100

    v0.1.1
    - #2
    - installed apps - task app

--- tm_db/settings.py
+++ tm_db/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'task.apps.TaskConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'tm_db.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'task_db',
+        'USER': 'postgres',
+        'PASSWORD': 'watermelonIce',
+        'HOST': 'localhost'
     }
 }
 
