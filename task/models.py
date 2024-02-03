ddfeade4a93b85b01a92fcad2995a8d55cca430f
bmokr <56951744+bmokr@users.noreply.github.com>
2024-02-03 19:55:16 +0100

    v0.4
    - started #5

--- task/models.py
+++ task/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
+from simple_history.models import HistoricalRecords
 
-# Create your models here.
+
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('new', 'New'),
+        ('ongoing', 'Ongoing'),
+        ('finished', 'Finished'),
+    ]
+
+    title = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    user = models.ForeignKey(User, blank=True, on_delete=models.DO_NOTHING)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='new')
+    list_date = models.DateTimeField(default=datetime.now, blank=True)
+
+    history = HistoricalRecords()
+
+    def __str__(self):
+        return self.title
